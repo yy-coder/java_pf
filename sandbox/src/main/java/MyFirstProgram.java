e480ab06ad6f5250c3d37a0c4334d30533d8f5d9
yy-coder <52197477+yy-coder@users.noreply.github.com>
2019-06-26 15:57:21 +0300

    Создан проект для IDEA

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,8 @@
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+    }
 }
