8fe035ec93738ab107fb0788d0ae3a138266bcb0
yy-coder <52197477+yy-coder@users.noreply.github.com>
2019-07-03 18:38:23 +0300

    добавление классов и конструкторов

--- sandbox/src/main/java/ru/stqu/pf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqu/pf/sandbox/MyFirstProgram.java
@@ -3,25 +3,24 @@ package ru.stqu.pf.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
         hello("world");
-        hello("NLurlan");
+        hello("Nurlan");
 
-        double len = 5;
-        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody);
     }
 
-    public static double area(double l) {
-        return l * l;
+    public static double area(Square s) {
+        return s.l * s.l;
     }
 
-    public static double area(double a, double b) {
-        return a * b;
+    public static double area(Rectangle r) {
+        return r.a * r.b;
     }
 }
---
+++ sandbox/src/main/java/ru/stqu/pf/sandbox/Square.java
@@ -0,0 +1,10 @@
+package ru.stqu.pf.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+}
